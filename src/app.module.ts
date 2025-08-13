535520e381bd8c5cafbb155a150ee15f6edcbacb
Mahisa Ajy Kusuma <mahisaajy@gmail.com>
2025-08-13 21:40:01 +0700

    feat: implement Weather module with controller and service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { WeatherModule } from './weather/weather.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // biar gak perlu import di setiap module
     }),
+    WeatherModule,
   ],
   controllers: [AppController],
   providers: [AppService],
